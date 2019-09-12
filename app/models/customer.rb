d63a29b409bf53cf7bbb4f80f2c161fa500dbcc4
Combos93 <combos93@mail.ru>
2019-09-12 17:21:54 +0600

    create model customer

---
+++ app/models/customer.rb
@@ -0,0 +1,29 @@
+class Customer < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :phone, presence: true, format: { with: /\A(([0-9][\- ]{1}|\+7[\- ]?|\+\d{3}|)[\- ]?)?(\(?\d{1,8}\)?[\- ]?)?[\d\- ]{5,10}\Z/, message: "phone format +7 916 1113344" }
+
+  validates :phone, uniqueness: true
+
+  scope :available, -> { where(blacklisted: false) }
+  scope :blocked, -> { where(blacklisted: true }
+
+  def to_blacklist!
+    self.blacklisted = true
+    save
+  end
+
+  def blacklist_off!
+    self.blacklisted = false
+    save
+  end
+
+  def black?
+    blacklisted
+  end
+
+  def self.search(phone)
+    available.where(phone: phone.strip).first
+  end
+end
+
